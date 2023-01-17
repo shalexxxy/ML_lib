81e08f8210f246e886b6c5b56ba4a8bab0ed66bb
Alexander <40820235+shalexxxy@users.noreply.github.com>
2023-01-17 21:02:48 +0300

    RF

--- ML.py
+++ ML.py
@@ -1,34 +1,38 @@
 import numpy as np
 import pandas as pd
 import funcs as f
+from random import randrange
 
+ # Linear Regression model (can be used 2 different ways of optimozation :
+ # gradient descent for custom function or method of least squares with MSE loss func)
 class LinearRegression:
-    def __init__(self, reg=None):
-        self.reg = reg
+    def __init__(self):
+        pass
 
     def calc_grad(self, x, y, func, step=0.0001):
-        grad = np.array([0 for i in range(self.weights.shape[0])])
-
+        # x - arguments, y - target, func - loss, step - gradient step
+        grad = np.array([0 for _ in range(self.weights.shape[0])])
         for i in range(len(self.weights)):
-            weights = self.weights.copy()
-            weights[i] += step
-            grad[i] = (func(x, y, weights) - func(x, y, self.weights)) / step
+            weights = self.weights.copy() # Defining copy of weights vector
+            weights[i] += step # adding step
+            grad[i] = (func(x, y, weights) - func(x, y, self.weights)) / step # calc grad
         return grad
-
+# fitting thw model for method of least squares
     def fit(self, x_train, y_train):
-        x_train = np.c_[x_train, np.ones(x_train.shape[0])]
+        x_train = np.c_[x_train, np.ones(x_train.shape[0])] # adding free term
         a = x_train.T.dot(x_train)
         b = x_train.T.dot(y_train)
-        self.weights = np.linalg.solve(a, b)
-
+        self.weights = np.linalg.solve(a, b) # solving the equation
+ # fitting model with custom loss
     def fit_custom(self, x, y, learning_rate, func=f.mse):
         self.weights = np.random.normal(0, 1, len(x[0]) + 1)
         loss = np.inf
         current_loss = func(x, y, self.weights)
+        # calculating grad steps until difference between loss funcs will be < 10**8 %
         while (abs(loss - current_loss) / current_loss) > 10 ** (-10):
             loss = current_loss
             grad = self.calc_grad(x, y, func)
-            self.weights = self.weights - grad * learning_rate
+            self.weights = self.weights - grad * learning_rate # adding anti-grad with lr
             #   print(self.weights)
             current_loss = func(x, y, self.weights)
 
@@ -36,28 +40,30 @@ class LinearRegression:
         x = np.c_[x, np.ones(x.shape[0])]
         return x.dot(self.weights.T)
 
-
+# tree node class
 class Node:
     def __init__(self):
-        self.split_val = None
-        self.depth = None
-        self.feature_num = None
+        self.split_val = None # value of the feature_num to split
+        self.depth = None # depth of the current node
+        self.feature_num = None # number of the feature to split
         self.left = None
         self.right = None
-        self.mass = None
+        self.mass = None # mass of target values in the node
+
 
 class ClassificationTree:
     def __init__(self):
         self.root = Node()
         self.root.depth = 1
 
-
+# Recursive func for split
     def split_current(self, data, current_node, min_elems, max_depth):
         x = data.loc[:, data.columns != 'labels']
         cols = x.columns
         min_gini_split = np.inf
         min_col = 1
         min_val = - np.inf
+        # for each unique value of the feature calc gini and tring to find the best split
         for i in cols:
             for j in list(sorted(data[i].unique())):
                 metric = (data[data[i] >= j].shape[0] / data.shape[0]) * f.gini(data[data[i] >= j]['labels']) + \
@@ -66,7 +72,7 @@ class ClassificationTree:
                     min_gini_split = metric
                     min_col = i
                     min_val = j
-
+        # creating new Nodes for split
         current_node.right = Node()
         current_node.left = Node()
         current_node.right.depth = current_node.depth + 1
@@ -75,14 +81,12 @@ class ClassificationTree:
         current_node.right.mass = data[data[min_col] >= min_val]
         current_node.feature_num = min_col
         current_node.split_val = min_val
-        print('Current depth',  current_node.right.depth)
-        print('Current split ', min_gini_split)
-        print(min_col, min_val)
+        # if new node satisfies requirements - we continue splitting
         if (current_node.right.mass.shape[0] > min_elems) and (current_node.right.mass['labels'].nunique() > 1) and (current_node.right.depth < max_depth):
             self.split_current(current_node.right.mass, current_node.right, min_elems, max_depth)
         if (current_node.left.mass.shape[0] > min_elems) and (current_node.left.mass['labels'].nunique() > 1) and (current_node.left.depth < max_depth):
             self.split_current(current_node.left.mass, current_node.left, min_elems, max_depth)
-
+    # fit func
     def fit(self, x_train, y_train, min_leaf_elems = 10, max_depth = 10):
         data = pd.DataFrame(x_train)
         data['labels'] = y_train
@@ -94,9 +98,9 @@ class ClassificationTree:
     def predict(self, x):
         x = pd.DataFrame(x)
         res = []
-        for i in x.index:
+        for i in x.index: # itarating rows of features
             current_node = self.root
-            while current_node is not None:
+            while current_node is not None: # walking until reaching the list
                 if current_node.right is None or current_node.left is None:
                     mass = current_node.mass['labels']
                     current_node = None
@@ -105,12 +109,13 @@ class ClassificationTree:
                         current_node = current_node.left
                     elif x.loc[i, current_node.feature_num] >= current_node.split_val and current_node.right is not None:
                         current_node = current_node.right
-
+           # print(mass)
+            # as a prediction we choose the most frequent label
             res.append(mass.value_counts().idxmax())
         return res
 
 
-
+## Same as classification tree
 class RegressionTree:
     def __init__(self):
         self.root = Node()
@@ -172,7 +177,70 @@ class RegressionTree:
                         current_node = current_node.right
 
             res.append(np.mean(mass))
-            if np.mean(mass) is None:
-
-                print('None is: ', mass)
         return res
+
+# Making and fitting num_trees of RegTrees, the prediction is mean value of predictions
+class RandomRegressionForest:
+    def __init__(self, max_depth = 5, min_leaf_elems = 5, num_trees = 10):
+        self.max_depth = max_depth
+        self.min_leaf_elems = min_leaf_elems
+        self.num_trees = num_trees
+        self.forest = [RegressionTree() for _ in range(num_trees)]
+
+    def fit(self, x_train, y_train):
+        self.parms = []
+        data = pd.DataFrame(x_train)
+        num_samples = data.shape[0]//self.num_trees
+        data['target'] = y_train
+        x = None
+        for i in range(len(self.forest)):
+            data_train = data.sample(num_samples)
+            target = data_train.pop('target')
+            x = data_train.sample(randrange(1,len(data_train.columns)), axis=1)
+            self.parms.append(x.columns)
+            self.forest[i].fit(x, target, self.min_leaf_elems, self.max_depth)
+
+    def predict(self, x):
+        self.predictions = []
+        x = pd.DataFrame(x)
+        for i in range(len(self.forest)):
+            self.predictions.append(np.array (self.forest[i].predict(x[list(self.parms[i]) ])))
+        res = sum(self.predictions)/self.num_trees
+        return res
+
+class RandomClassificationForest:
+    def __init__(self, max_depth=5, min_leaf_elems=10, num_trees = 5):
+        self.max_depth = max_depth
+        self.min_leaf_elems = min_leaf_elems
+        self.num_trees = num_trees
+        self.forest = [ClassificationTree() for _ in range(num_trees)]
+
+    def fit(self, x_train, y_train):
+        self.parms = []
+        data = pd.DataFrame(x_train)
+        num_samples = data.shape[0]//self.num_trees
+        data['labels'] = y_train
+        for i in range(len(self.forest)):
+            data_train = data.sample(num_samples)
+            target = data_train.pop('labels')
+            x = data_train.sample(randrange(1,len(data_train.columns)), axis=1)
+            self.parms.append(x.columns)
+            self.forest[i].fit(x, target, self.min_leaf_elems, self.max_depth)
+        print('model was fitted')
+
+    def predict(self, x):
+        self.predictions = []
+        x = pd.DataFrame(x)
+        for i in range(len(self.forest)):
+            self.predictions.append(np.array (self.forest[i].predict(x[list(self.parms[i]) ])))
+            print(i, ' predicted')
+        self.predictions  = pd.DataFrame(self.predictions).T
+        res = []
+        print(self.predictions)
+        for i in list(self.predictions.index):
+            val = self.predictions.loc[i, :].value_counts().idxmax()
+            res.append(val)
+            print(val)
+        print(res)
+        return res
+
--- test.py
+++ test.py
@@ -6,17 +6,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_boston
 from sklearn.tree import DecisionTreeRegressor, _criterion
 
-x = pd.DataFrame(load_boston().data)
-y = pd.DataFrame(load_boston().target)
-    #pd.read_csv('Covid Data.csv')
-
-# x = data[['SEX', 'TOBACCO', 'PREGNANT',	'DIABETES',	'COPD',	'ASTHMA',	'INMSUPR',	'HIPERTENSION',	'OTHER_DISEASE',	'CARDIOVASCULAR',	'OBESITY',	'RENAL_CHRONIC'
-# ]]
-# y = data['CLASIFFICATION_FINAL']
+#x = pd.DataFrame(load_boston().data)
+#y = pd.DataFrame(load_boston().target)
+data = pd.read_csv('Covid Data.csv').dropna()
+x = data[['SEX', 'TOBACCO', 'PREGNANT',	'DIABETES',	'COPD',	'ASTHMA',	'INMSUPR',	'HIPERTENSION',	'OTHER_DISEASE',	'CARDIOVASCULAR',	'OBESITY',	'RENAL_CHRONIC'
+]]
+y = data['CLASIFFICATION_FINAL']
 #
 x = x.to_numpy()
+
 y = y.to_numpy()
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.33, random_state=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
 
 
 #
@@ -27,12 +27,12 @@ x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.33, random_s
 # x[:,4] = x[:,4]/np.linalg.norm(x[:,4])
 
 #
-model_def = DecisionTreeRegressor(max_depth=10, min_samples_leaf=10, criterion='mae')
-model = RegressionTree()
-model.fit(x_train,y_train, max_depth=10)
-model_def.fit(x_train,y_train)
+model = RandomClassificationForest()
+#model = ClassificationTree()
+model.fit(x_train,y_train)
+#model_def.fit(x_train,y_train)
 pred = model.predict(x_test)
-pred2 = model_def.predict(x_test)
+#pred2 = model_def.predict(x_test)
 #print(mean_absolute_percentage_error(y_test, pred))
-print('My model MAE: ', mean_absolute_error(y_test,np.array(pred)))
-print('sklearn model MAE: ', mean_absolute_error(y_test,np.array(pred2)))
+print('My model MAE: ', accuracy_score(y_test,np.array(pred)))
+#print('sklearn model MAE: ', mean_absolute_error(y_test,np.array(pred2)))
